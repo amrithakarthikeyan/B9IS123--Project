19487ac96ea16e11a3c099fa09cadb45b9f1006a
amrithakarthikeyan <amrithakv4s@gmail.com>
2025-06-30 11:55:31 +0000

    Function loadAssets: script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -5,3 +5,29 @@ const cancelEditBtn = document.getElementById('cancelEdit');
 const API_URL = 'https://expert-space-dollop-wrxjwq5xrwj4c5vw6-3000.app.github.dev/';
 
 console.log("Script loaded");
+
+function loadAssets() {
+  fetch(API_URL + 'assets')
+    .then(res => res.json())
+    .then(data => {
+      tableBody.innerHTML = '';
+      data.forEach(asset => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${asset['Asset-ID']}</td>
+          <td>${asset['Asset-Type']}</td>
+          <td>${asset.Brand}</td>
+          <td>${asset.Model}</td>
+          <td>${asset['Serial-Number']}</td>
+          <td>${asset.Purchase_Date}</td>
+          <td>${asset.Status}</td>
+          <td>
+            <button onclick="editAsset(${asset['Asset-ID']})">Edit</button>
+            <button onclick="deleteAsset(${asset['Asset-ID']})">Delete</button>
+          </td>`;
+        tableBody.appendChild(row);
+      });
+    });
+}
+
+
