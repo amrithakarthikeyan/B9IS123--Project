42ba42a2913de84ef0bee12cd1b1490c5eb0e078
amrithakarthikeyan <amrithakv4s@gmail.com>
2025-07-02 17:17:51 +0000

    declaring table in server and route messagefor the backend- server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,27 @@
 const express = require('express');
-const cors = require('cors');
 
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const PORT = 3000;
 
+const cors = require('cors');
+
 app.use(cors());
+
+app.use(express.json());
+
+const db = new sqlite3.Database('./database.db');
+//db.run (`DROP TABLE assets`);
+db.run(`CREATE TABLE IF NOT EXISTS assets (
+  "Asset-ID" INTEGER PRIMARY KEY,
+  "Asset-Type" TEXT NOT NULL,
+  "Brand" TEXT,
+  "Model" TEXT,
+  "Serial-Number" INTEGER,
+  "Purchase_Date" TEXT,
+  "Status" TEXT
+)`);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Sparkout Tech Info System API');
+});
