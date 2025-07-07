f605fe817737981b5123cecaf3deb6b8a19ed264
amrithakarthikeyan <amrithakv4s@gmail.com>
2025-07-07 12:33:07 +0000

    Updated database with table assets

--- Backend/assetsdata.sql
+++ Backend/assetsdata.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS Assets;
+
 CREATE TABLE "Assets" (
 	"Asset-ID"	INTEGER,
 	"Asset-Type"	TEXT NOT NULL,
@@ -7,23 +9,4 @@ CREATE TABLE "Assets" (
 	"Purchase_Date"	TEXT,
 	"Status"	TEXT,
 	PRIMARY KEY("Asset-ID")
-);
-
-CREATE TABLE "Assignment" (
-	"Assignment_ID"	INTEGER NOT NULL PRIMARY KEY,
-	"Asset-ID"	INTEGER NOT NULL,
-	"Employee_ID"	INTEGER NOT NULL,
-	"Assigned_Date"	TEXT,
-	"Return_Date"	TEXT,
-	"Condition_on_return"	TEXT,
-	FOREIGN KEY("Asset-ID") REFERENCES "Assets"("Asset-ID") ,
-	FOREIGN KEY("Employee_ID") REFERENCES "Employees"("Employee-ID")
-);
-
-CREATE TABLE "Employees" (
-	"Employee-ID"	INTEGER NOT NULL,
-	"Name"	TEXT NOT NULL,
-	"Department"	INTEGER,
-	"Email"	TEXT UNIQUE,
-	PRIMARY KEY("Employee-ID")
 );
